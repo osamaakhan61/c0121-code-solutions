d4322321e8bd1d842cbb3529652d3902ff47a1ce
Osama Khan <osamaa.khan61@gmail.com>
2021-01-25 10:33:19 -0800

    Defined variables and console logs with labels

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+const fullName = 'Osama Khan';
+const isCool = true;
+const totalPets = 2;
+
+console.log('Full Name: ', fullName);
+console.log('Am I Cool? ', isCool);
+console.log('Number of pets: ', totalPets);
+
+console.log('Type for name: ', typeof (fullName));
+console.log('Type for cool: ', typeof (isCool));
+console.log('Type for pets: ', typeof (totalPets));
+
+const unknown = null;
+
+console.log('Unknown: ', unknown);
+console.log('Type for unknown: ', typeof (unknown));
+
+let never;
+
+console.log('Never: ', never);
+console.log('Type for never: ', typeof (never));
