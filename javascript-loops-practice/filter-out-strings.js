7c6147fd24829b9dea2fec08fa82c871ce2cb23a
Osama Khan <osamaa.khan61@gmail.com>
2021-01-29 10:30:45 -0800

    Inverted logic for if statement

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,10 +2,7 @@
 function filterOutStrings(values) {
   var noStrings = [];
   for (var i = 0; i < values.length; i++) {
-    if (typeof (values[i]) === 'string') {
-      // eslint-disable-next-line no-void
-      void (0);
-    } else {
+    if (typeof (values[i]) !== 'string') {
       noStrings.push(values[i]);
     }
   }
