e5e18dc76d30da655469e198b2df85889562e622
Osama Khan <osamaa.khan61@gmail.com>
2021-02-01 10:40:55 -0800

    removed duplicates

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -2,26 +2,16 @@ console.log('Hello, world!');
 
 document.querySelector('h1');
 
-console.log('Query for h1: ', document.querySelector('h1'));
-
 console.dir(document.querySelector('h1'));
 
 document.querySelector('#explanation');
-
-console.log('Query for explanation: ', document.querySelector('#explanation'));
 
 console.dir(document.querySelector('#explanation'));
 
 document.querySelector('.hint');
 
-console.log('Query for hint: ', document.querySelector('.hint'));
-
 console.dir(document.querySelector('.hint'));
 
 document.querySelectorAll('p');
 
-console.log(document.querySelectorAll('p'));
-
 document.querySelectorAll('.example-link');
-
-console.log(document.querySelectorAll('.example-link'));
