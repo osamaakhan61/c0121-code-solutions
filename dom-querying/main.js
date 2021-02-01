eb25b2b8ec41e8015e22d77fe4bb8884c6b7f382
Osama Khan <osamaa.khan61@gmail.com>
2021-02-01 11:07:23 -0800

    Cleaned it up

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -2,16 +2,26 @@ console.log('Hello, world!');
 
 document.querySelector('h1');
 
+console.log('$heading ', document.querySelector('h1'));
+
 console.dir(document.querySelector('h1'));
 
 document.querySelector('#explanation');
+
+console.log('$explanation ', document.querySelector('#explanation'));
 
 console.dir(document.querySelector('#explanation'));
 
 document.querySelector('.hint');
 
+console.log('$hint ', document.querySelector('.hint'));
+
 console.dir(document.querySelector('.hint'));
 
 document.querySelectorAll('p');
 
+console.log('$paragraphs ', document.querySelectorAll('p'));
+
 document.querySelectorAll('.example-link');
+
+console.log('$links ', document.querySelectorAll('.example-link'));
