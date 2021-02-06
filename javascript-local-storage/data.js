a51d84d52d5aaf4d1cda4bab5b940c28c1bbf49b
Osama Khan <osamaa.khan61@gmail.com>
2021-02-05 17:10:04 -0800

    Added conditional for null

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,7 +1,9 @@
 /* exported todos */
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
-todos = JSON.parse(previousTodosJSON);
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
 window.addEventListener('beforeunload', function (event) {
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
