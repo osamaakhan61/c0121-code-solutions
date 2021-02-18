3797e9270692c0d4ff00793099f5a52dc53f79ef
Osama Khan <osamaa.khan61@gmail.com>
2021-02-18 09:57:35 -0800

    Added Chevron click js

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,17 +1,22 @@
 // change dot shades
 var grabDots = document.querySelectorAll('i.fa-circle');
+var grabImages = document.querySelectorAll('img');
 var leftChev = document.querySelector('i.fa-chevron-left');
 var rightChev = document.querySelector('i.fa-chevron-right');
 var startingIndex = 0;
+dotClick(startingIndex);
+
+function ChevClick(i) {
+  dotClick(startingIndex += i);
+}
 
 function dotClick(i) {
   if (grabDots[i].className === 'far fa-circle') {
     for (var j = 0; j < grabDots.length; j++) {
       grabDots[j].className = 'far fa-circle';
+      grabImages[j].className = 'hidden pokemon';
     }
     grabDots[i].className = 'fas fa-circle';
+    grabImages[i].className = 'pokemon noSelect';
   }
 }
-
-leftChev.addEventListener('click', dotClick(startingIndex--));
-rightChev.addEventListener('click', dotClick(startingIndex++));
