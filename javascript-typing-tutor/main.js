be33798a0d216580aa0330e7b926f48e2fd7ad3d
khan-osama <osamaa.khan61@gmail.com>
2021-03-12 14:34:10 -0800

    saving progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,5 +1,21 @@
 const phraseArray = document.querySelectorAll('span');
+let lettersCorrect = 0;
 
 window.addEventListener('keydown', function (e) {
-  console.log(String.fromCharCode(e.keyCode).toLowerCase());
+  const typedLetter = String.fromCharCode(e.keyCode).toLowerCase();
+  for (var i = 0; i < phraseArray.length; i++) {
+    if (phraseArray[i].innerHTML === typedLetter) {
+      if (phraseArray[i].className === 'correct-char') {
+        continue;
+      } else if ((phraseArray[i].className !== 'correct-char' && phraseArray[i - 1] === undefined) || phraseArray[i - 1].className === 'correct-char') {
+        phraseArray[i].className = 'correct-char';
+        phraseArray[i + 1].classList.add('current-char');
+        lettersCorrect++;
+      }
+    } else {
+      if (phraseArray[i].className === 'current-char' && typedLetter !== phraseArray[i].textContent) {
+        phraseArray[i].className = 'incorrect-char';
+      }
+    }
+  }
 });
