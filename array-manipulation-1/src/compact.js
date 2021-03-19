a1ab83edca14454ef6f6ef8b30902132bfed20ed
khan-osama <osamaa.khan61@gmail.com>
2021-03-18 20:46:15 -0700

    Completed challenges

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,12 +1,9 @@
 /* exported compact */
 function compact(array) {
-  var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === false || array[i] === null || array[i] === NaN || array[i] === 0 || array[i] === -0 || array[i] === undefined || array[i] === '') {
-      array[i].pop();
-    } else {
-      newArray.push(array[i]);
-    }
-    return newArray;
+  const newArray = [];
+  for (const i of array) {
+    i && newArray.push(i);
   }
+  array = newArray;
+  return array;
 }
