402aa567ca87063a34836244f40ffde9c6a0a772
khan-osama <osamaa.khan61@gmail.com>
2021-03-22 11:37:56 -0700

    Fixed algorithms

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -2,11 +2,9 @@
 function capitalizeWord(word) {
   const newWord = word.toLowerCase();
   const capWord = newWord.charAt(0).toUpperCase() + newWord.slice(1);
-  for (var i = 0; i < capWord.length; i++) {
-    if (capWord[i] === 's') {
-      const finalCase = capWord.charAt(i).toUpperCase() + capWord.slice(i + 1);
-      return capWord.substring(0, 4) + finalCase;
-    }
+  if (capWord === 'Javascript') {
+    const finalCase = capWord.charAt(4).toUpperCase() + capWord.slice(5);
+    return capWord.substring(0, 4) + finalCase;
   }
   return capWord;
 }
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,7 +1,6 @@
 /* exported numVowels */
 function numVowels(string) {
   string = string.toLowerCase();
-  string = string.split('');
   let vowelCounter = 0;
   for (var i = 0; i < string.length; i++) {
     if (string[i] === 'a' || string[i] === 'e' || string[i] === 'i' || string[i] === 'o' || string[i] === 'u') {
