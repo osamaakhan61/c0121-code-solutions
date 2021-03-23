b17fd8d13fe868178cc0b2c2dd38e8df03d60bb3
khan-osama <osamaa.khan61@gmail.com>
2021-03-23 14:48:23 -0700

    Completed challenge

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -3,19 +3,11 @@ let lettersCorrect = 0;
 
 window.addEventListener('keydown', function (e) {
   const typedLetter = String.fromCharCode(e.keyCode).toLowerCase();
-  for (var i = 0; i < phraseArray.length; i++) {
-    if (phraseArray[i].innerHTML === typedLetter) {
-      if (phraseArray[i].className === 'correct-char') {
-        continue;
-      } else if ((phraseArray[i].className !== 'correct-char' && phraseArray[i - 1] === undefined) || phraseArray[i - 1].className === 'correct-char') {
-        phraseArray[i].className = 'correct-char';
-        phraseArray[i + 1].classList.add('current-char');
-        lettersCorrect++;
-      }
-    } else {
-      if (phraseArray[i].className === 'current-char' && typedLetter !== phraseArray[i].textContent) {
-        phraseArray[i].className = 'incorrect-char';
-      }
-    }
+  if (phraseArray[lettersCorrect].innerHTML === typedLetter) {
+    phraseArray[lettersCorrect].className = 'correct-char';
+    lettersCorrect++;
+    phraseArray[lettersCorrect].className = 'current-char';
+  } else if (phraseArray[lettersCorrect].innerHTML !== typedLetter) {
+    phraseArray[lettersCorrect].className = 'incorrect-char';
   }
 });
