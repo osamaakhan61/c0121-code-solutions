210ec99dfc58d74d826118679677df311a8055da
khan-osama <osamaa.khan61@gmail.com>
2021-03-23 15:46:30 -0700

    saving progress

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,27 +1,26 @@
 /* exported Account */
 function Account(number, holder) {
-  this.number = number,
-  this.holder = holder,
+  this.number = number;
+  this.holder = holder;
   this.transactions = [];
 }
-/* this.deposit = function (amount) {
-    if (amount > 0) {
-      this.transactions.unshift(Transaction('deposit', amount));
-      return true;
-    } else {
-      return false;
-    }
-  },
-  this.withdraw = function (amount) {
-    if (amount > 0) {
-      this.transactions.unshift(Transaction('withdrawal', amount));
-      return true;
-    } else {
-      return false;
-    }
-  },
-  this.getBalance = function () {
-    if (this.transactions === undefined) {
-      return 0;
-    }
-  }; */
+
+Account.prototype.deposit = function (amount) {
+  if (amount > 0) {
+    this.transactions.push(Transaction('deposit', amount));
+    return true;
+  }
+  return false;
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (amount > 0) {
+    this.transactions.push(Transaction('withdrawal', amount));
+    return true;
+  }
+  return false;
+};
+
+Account.prototype.getBalance = function () {
+
+};
