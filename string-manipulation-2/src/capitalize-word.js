fcc09bbea55f374288fbf0107c1444d0add08463
khan-osama <osamaa.khan61@gmail.com>
2021-03-24 17:07:40 -0700

    Revised capitalize word js

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -3,8 +3,7 @@ function capitalizeWord(word) {
   const newWord = word.toLowerCase();
   const capWord = newWord.charAt(0).toUpperCase() + newWord.slice(1);
   if (capWord === 'Javascript') {
-    const finalCase = capWord.charAt(4).toUpperCase() + capWord.slice(5);
-    return capWord.substring(0, 4) + finalCase;
+    return 'JavaScript';
   }
   return capWord;
 }
