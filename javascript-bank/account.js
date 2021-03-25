dac382007c490dbe973b5f939554f9cbf4b842e9
khan-osama <osamaa.khan61@gmail.com>
2021-03-24 20:28:38 -0700

    completed js bank

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -7,7 +7,7 @@ function Account(number, holder) {
 
 Account.prototype.deposit = function (amount) {
   if (amount > 0) {
-    this.transactions.push(Transaction('deposit', amount));
+    this.transactions.push(new Transaction('deposit', amount));
     return true;
   }
   return false;
@@ -15,12 +15,20 @@ Account.prototype.deposit = function (amount) {
 
 Account.prototype.withdraw = function (amount) {
   if (amount > 0) {
-    this.transactions.push(Transaction('withdrawal', amount));
+    this.transactions.push(new Transaction('withdrawal', amount));
     return true;
   }
   return false;
 };
 
 Account.prototype.getBalance = function () {
-
+  var totalBalance = 0;
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      totalBalance += this.transactions[i].amount;
+    } else {
+      totalBalance -= this.transactions[i].amount;
+    }
+  }
+  return totalBalance;
 };
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,39 @@
 /* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance <= 0 || Number.isInteger(balance) !== true) {
+    return null;
+  } else {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.transactions.push(new Transaction('deposit', balance));
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  if (this.accounts[number - 1] === undefined) {
+    return null;
+  } else {
+    return this.accounts[number - 1];
+  }
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var totalBalance = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    for (var j = 0; j < this.accounts[i].transactions.length; j++) {
+      if (this.accounts[i].transactions[j].type === 'deposit') {
+        totalBalance += this.accounts[i].transactions[j].amount;
+      } else {
+        totalBalance -= this.accounts[i].transactions[j].amount;
+      }
+    }
+  }
+  return totalBalance;
+};
