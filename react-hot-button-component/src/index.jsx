7d48d8a0a49f3c251f6327b6a255dab17b9621f4
khan-osama <osamaa.khan61@gmail.com>
2021-03-26 11:10:52 -0700

    Saving progress

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonTemp: 0
+    };
+  }
+
+}
