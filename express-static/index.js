3ffd074369ced25339d11e5395e8829d3038ccd6
khan-osama <osamaa.khan61@gmail.com>
2021-03-29 10:53:45 -0700

    comepleted assignment

---
+++ express-static/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const absPath = path.join(__dirname, 'public');
+const fileServe = express.static(absPath);
+
+app.use(fileServe);
+
+app.listen(3000, () => {
+  console.log('Listening very intently to your request!');
+  console.log(absPath);
+});
