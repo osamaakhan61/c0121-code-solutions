4077d89934675deb3e8524e9532bd828fadbac4b
khan-osama <osamaa.khan61@gmail.com>
2021-06-21 11:55:45 -0700

    finished

---
+++ javascript-functions/main.js
@@ -0,0 +1,19 @@
+function convertMinutesToSeconds(minutes) {
+  return minutes * 60;
+}
+
+function greet(name) {
+  return 'Hey ' + name;
+}
+
+function getArea(width, height) {
+  return width * height;
+}
+
+function getFirstName(person) {
+  return person.firstName;
+}
+
+function getLastElement(array) {
+  return array[array.length - 1];
+}
